5752580755ae9bd40898604d4727f624549e47b1
mattdvhope <mattagape@me.com>
2020-08-03 13:43:38 +0700

    Removing login

--- src/components/banner.js
+++ src/components/banner.js
@@ -79,7 +79,7 @@ export default class Banner extends Component {
               <div className="banner-details">
                 <span style={{ fontSize: `${window.screen.width > 600 ? 8 : 10}vw` }}>
                   {data.name}
-                </span> {/* <h1>I'm {data.designation}.</h1> */} 
+                </span>
                 <span style={{
                   fontSize: `${window.screen.width > 600 ? 4 : 7.5}vw`,
                   marginLeft: `auto`,
