fa82e280835a12400c11931fbb022b2755b2b252
mattdvhope <mattagape@me.com>
2020-08-03 12:40:50 +0700

    Added Link

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "gatsby";
 import Img from "gatsby-image";
 import { isLoggedIn } from "../utils/auth"
 // import { linkVisit } from "../utils/railsVisits"
