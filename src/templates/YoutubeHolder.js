1a9dd7822be5ba6b66c4196b213e07e7e0909200
mattdvhope <mattagape@me.com>
2020-08-26 19:05:02 +0700

    Moved all graphql to blog-post

--- src/templates/YoutubeHolder.js
+++ src/templates/YoutubeHolder.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react"
-import { StaticQuery, graphql } from "gatsby";
 import { youtubeEmbeddable } from "../utils/youtubeEmbeddable"
 import FormYoutubeSurvey from "./formYoutubeSurvey"
 
