78a37fc615118a2ba3fdf6521f6b561de71eec2f
mattdvhope <mattagape@me.com>
2020-08-26 19:02:20 +0700

    Moved all graphql to blog-post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -7,66 +7,46 @@ import YoutubeHolder from "./YoutubeHolder"
 import FacebookPageBox from "../components/FacebookPageBox"
 
 export default class blogPost extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      window: undefined
-    };
-  }
-
-  componentDidMount() {
-    this.setState({ window: window })
-    console.log(this.props)
-  }
-
   render() {
-    if (this.state.window) {
+    const data = this.props.data.contentfulBlogs;
+    const siteurl = this.props.data.contentfulHomePage.siteUrl + "/";
+    const socialConfigss = {
+      site: {
+        siteMetadata: { siteurl }
+      },
+      title: data.title,
+      slug: data.slug
+    };
 
-      const data = this.props.data.contentfulBlogs;
-      const siteurl = this.props.data.contentfulHomePage.siteUrl + "/";
-      const socialConfigss = {
-        site: {
-          siteMetadata: { siteurl }
-        },
-        title: data.title,
-        slug: data.slug
-      };
-
-console.log(data)
-
-      return (
-        <Layout>
-          <SEO
-            title={data.title}
-            keywords={[
-              `สายสัมพันธ์ ความสุข`,
-              `Frontend Developer`,
-              `Developer`,
-              `${data.title}`
-            ]}
-          />
-          <div className="site-container blog-post">
-            <div className="container">
-              <YoutubeHolder data={data} />
-              <FacebookPageBox/>
-              <br/>
-              <hr/>
-              <Share
-                socialConfig={{
-                  config: {
-                    url: `${siteurl}${socialConfigss.slug}`,
-                    title: `${socialConfigss.title}`
-                  }
-                }}
-              />
-            </div>
+    return (
+      <Layout>
+        <SEO
+          title={data.title}
+          keywords={[
+            `สายสัมพันธ์ ความสุข`,
+            `Frontend Developer`,
+            `Developer`,
+            `${data.title}`
+          ]}
+        />
+        <div className="site-container blog-post">
+          <div className="container">
+            <YoutubeHolder data={data} />
+            <FacebookPageBox/>
+            <br/>
+            <hr/>
+            <Share
+              socialConfig={{
+                config: {
+                  url: `${siteurl}${socialConfigss.slug}`,
+                  title: `${socialConfigss.title}`
+                }
+              }}
+            />
           </div>
-        </Layout>
-      );
-    } else {
-      return <span/>
-    }
+        </div>
+      </Layout>
+    );
   }
 }
 
