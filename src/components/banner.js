af5f88bd7b3984eb05d9f4cf714ecd682fd9336a
mattdvhope <mattagape@me.com>
2020-09-03 14:43:04 +0700

    Change link - this week's blog

--- src/components/banner.js
+++ src/components/banner.js
@@ -25,7 +25,7 @@ export default class Banner extends Component {
   GatsbyLink() {
     return (
       <a 
-        href={`who-is-jesus`}
+        href={`eating-the-fruit`}
         style={this.state.linkColor}
         onClick='FB.AppEvents.logEvent("GatsbyLinkClicked")'
       >
