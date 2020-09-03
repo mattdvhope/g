91b270cfa780840626c88e16037b30d2595fa375
mattdvhope <mattagape@me.com>
2020-09-03 15:01:14 +0700

    Commented out date

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -42,10 +42,10 @@ export default class Blogs extends Component {
                       )}
                       <div className="details">
                         <h3 className="title">{item.node.title}</h3>
-                        <span className="date">
+                  {/*   <span className="date">
                           <i className="fas fa-calendar-alt"></i>{" "}
                           {moment(item.node.createdAt).format("LL")}
-                        </span>
+                        </span>  */}
                       </div>
                     </div>
                   </li>
