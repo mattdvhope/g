57f93cd698caaf8eaf36aae0d12dc84116c60268
mattdvhope <mattagape@me.com>
2020-09-16 14:32:41 +0700

    Removed import

--- src/templates/YoutubeHolderTempSimple.js
+++ src/templates/YoutubeHolderTempSimple.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react"
 import { youtubeEmbeddable } from "../utils/youtubeEmbeddable"
-import FormYoutubeSurvey from "./formYoutubeSurvey"
 
 const YoutubeHolder = ({data}) => {
 
