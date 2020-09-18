2820b51cd1e35dbca05ac091e63f0aa585be936c
mattdvhope <mattagape@me.com>
2020-09-18 14:59:15 +0700

    Set up useState for setPrompt

--- src/templates/YoutubeHolderPrompts.js
+++ src/templates/YoutubeHolderPrompts.js
@@ -2,45 +2,40 @@ import React, { useState, useEffect } from "react"
 import { youtubeEmbeddable } from "../utils/youtubeEmbeddable"
 import YoutubeVideo from "./YoutubeVideo"
 import ButtonForPrompt from "./ButtonForPrompt"
-import ResponsePrompt from "./ResponsePrompt"
 
 const YoutubeHolderPrompts = ({data}) => {
 
   const prompts = data.promptsForResponse;
-  const firstPrompt = prompts[0];
   const lastPrompt = prompts[prompts.length-1];
-
-  // const [buttonPressed, setButtonState] = useState(false);
-  // const [buttonWords, setButtonWords] = useState(firstPrompt.buttonInvitation);
   const [promptsElementNum, setPrompt] = useState(0);
 
-
-
-  function DealWithButtonPressing() {
-    console.log("pressed!!!!")
-    // setPrompt(promptsElementNum + 1)
+  const DealWithButtonPressing = () => {
+    promptsElementNum !== prompts.length-1 ? setPrompt(promptsElementNum + 1) : setPrompt(promptsElementNum - 1);
   }
+
+  const EveryButLastElement = () => (
+    <div id="YoutubeHolderPrompts" className="container-fluid">
+      <h2 style={{ color: `#BF8F63` }}><i>{data.ctaFirst}</i></h2>
+      <hr/>
+      <div>
+        <YoutubeVideo src={youtubeEmbeddable(data.youtubeUrl)} />
+        <hr/>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: prompts[promptsElementNum].promptContent.childMarkdownRemark.html
+          }}
+        />
+        <hr/>
+        <ButtonForPrompt onClick={() => DealWithButtonPressing()} buttonWords={prompts[promptsElementNum].buttonInvitation} />
+        <br/>
+        <hr/>
+      </div>
+    </div>
+  )
 
   if (data) {
-    return (
-      <div id="YoutubeHolderPrompts" className="container-fluid">
-        <h2 style={{ color: `#BF8F63` }}><i>{data.ctaFirst}</i></h2>
-        <hr/>
-  			<div>
-  				<YoutubeVideo src={youtubeEmbeddable(data.youtubeUrl)} />
-          <hr/>
-          <br/>
-
-          <ButtonForPrompt onClick={() => DealWithButtonPressing(true)} buttonWords={prompts[promptsElementNum].buttonInvitation} />
-
-          <br/>
-          <hr/>
-  		  </div>
-      </div>
-  	)
-  } else {
-    return null;
-  }
+    return EveryButLastElement();
+  } 
 
 }
 
