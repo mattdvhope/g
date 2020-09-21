c5674126a713208a8e43f124dde8bd75d2782708
mattdvhope <mattagape@me.com>
2020-09-21 10:09:36 +0700

    Added line

--- src/templates/YoutubeHolderPrompts.js
+++ src/templates/YoutubeHolderPrompts.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import { youtubeEmbeddable } from "../utils/youtubeEmbeddable"
 import YoutubeVideo from "./YoutubeVideo"
 import ButtonForPrompt from "./ButtonForPrompt"
@@ -6,14 +6,16 @@ import ButtonForPrompt from "./ButtonForPrompt"
 const YoutubeHolderPrompts = ({data}) => {
 
   const prompts = data.promptsForResponse;
-  const lastPrompt = prompts[prompts.length-1];
   const [promptsElementNum, setPrompt] = useState(0);
-
-  const DealWithButtonPressing = () => {
-    promptsElementNum !== prompts.length-1 ? setPrompt(promptsElementNum + 1) : setPrompt(promptsElementNum - 1);
+  const ButtonProvidedIfNeeded = () => {
+    if (promptsElementNum !== prompts.length-1) {
+      return (<ButtonForPrompt onClick={() => setPrompt(promptsElementNum + 1)} buttonWords={prompts[promptsElementNum].buttonInvitation} />)
+    } else {
+      return (<div dangerouslySetInnerHTML={{ __html: data.ctaLast.childMarkdownRemark.html }} />)
+    }
   }
 
-  const EveryButLastElement = () => (
+  return (
     <div id="YoutubeHolderPrompts" className="container-fluid">
       <h2 style={{ color: `#BF8F63` }}><i>{data.ctaFirst}</i></h2>
       <hr/>
@@ -26,14 +28,11 @@ const YoutubeHolderPrompts = ({data}) => {
           }}
         />
         <hr/>
-        <ButtonForPrompt onClick={() => DealWithButtonPressing()} buttonWords={prompts[promptsElementNum].buttonInvitation} />
-        <br/>
+        {ButtonProvidedIfNeeded()}
         <hr/>
       </div>
     </div>
   )
-
-    return EveryButLastElement();
 
 }
 
