0c401355e2db89a9a585b7ccbdd8051142c715e4
Matt Malone <mattagape@me.com>
2021-01-06 17:54:10 +0700

    Changed main link

--- src/components/banner.js
+++ src/components/banner.js
@@ -25,7 +25,7 @@ export default class Banner extends Component {
   GatsbyLink() {
     return (
       <a 
-        href={`i-am`}
+        href={`good-shepherd`}
         style={this.state.linkColor}
         onClick='FB.AppEvents.logEvent("GatsbyLinkClicked")'
       >
