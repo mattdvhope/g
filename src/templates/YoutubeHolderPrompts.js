930da7e3146d2afacb9b453fa8c4a95d2aabe010
Matt Malone <mattagape@me.com>
2021-04-29 15:32:27 +0700

    Set up FB Conversion Event post req.

--- src/templates/YoutubeHolderPrompts.js
+++ src/templates/YoutubeHolderPrompts.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react"
-import { youtubeEmbeddable } from "../utils/youtubeEmbeddable"
-import YoutubeVideo from "./YoutubeVideo"
-import ButtonForPrompt from "./ButtonForPrompt"
-import CtaUnderVideo from "./CtaUnderVideo"
+import React, { useState } from "react";
+import axios from 'axios';
+import qs from 'qs';
+import { youtubeEmbeddable } from "../utils/youtubeEmbeddable";
+import YoutubeVideo from "./YoutubeVideo";
+import ButtonForPrompt from "./ButtonForPrompt";
+import CtaUnderVideo from "./CtaUnderVideo";
 
 const YoutubeHolderPrompts = ({data}) => {
 
@@ -12,12 +14,48 @@ const YoutubeHolderPrompts = ({data}) => {
 
   const scrollToTopOfBlog = () => window.innerWidth < 1000 ? window.scrollTo(0, 0) : null
 
+  const FbConversionEvent = () => {
+    const data = 
+      {
+        "data": [
+            {
+                "event_name": "Reading",
+                "event_time": 1619683114,
+                "action_source": "website",
+                "event_source_url": "https://relationshipsthailand.org/arresting-jesus",
+                "user_data": {
+                    "fn": "78675cc176081372c43abab3ea9fb70c74381eb02dc6e93fb6d44d161da6eeb3",
+                    "ln": "6627835f988e2c5e50533d491163072d3f4f41f5c8b04630150debb3722ca2dd",
+                    "fb_login_id": 12345,
+                    "client_user_agent": "Matt"
+                },
+                "custom_data": {
+                    "currency": "THB",
+                    "value": 100
+                }
+            }
+        ]
+      };
+
+    const url = "https://graph.facebook.com/v10.0/222946198915072/events?access_token=EAACYmSXRUF0BAFdhl36TI7FrOUx9uEitzbuXWUZAIMYE0f4qiuNzYvKWZATdbWGeES5BE8ivcMR8GjDhVKIb0ZBuHznBdQ0wB7aUn5ZAaOZCim7f4LOVltFaxuOsg6rUVOxR5UxF3C9pbCYY7dDIFG0L3UcHhVRBgq5ZCa7DN00C1c8TLD0V8AutKXzRTIEZA4ZD";
+
+    axios.post(url, data)
+    .then(response => {
+      console.log(response)
+      return response.data.message;
+    })
+    .catch(function (error) {
+      // handle error
+      console.log("ERROR: ", error);
+    })
+  };
+
   const ButtonProvidedIfNeeded = () => {
     if (promptsElementNum === 0) {
       return (
         <div>
           <hr/>
-          <ButtonForPrompt onClick={() => { setPrompt(promptsElementNum + 1); scrollToTopOfBlog(); }} buttonWords={buttonWords} />
+          <ButtonForPrompt onClick={() => { setPrompt(promptsElementNum + 1); FbConversionEvent(); scrollToTopOfBlog(); }} buttonWords={buttonWords} />
         </div>)
     } else if (promptsElementNum !== prompts.length-1) {
       return (
