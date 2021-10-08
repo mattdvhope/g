a7ef641efbf600ed36a10dc5ff05fe8df25c557e
Matt Malone <mattagape@me.com>
2021-10-08 11:56:58 +0700

    Removed gatsby-plugin-facebook-analytics & commented out app

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,33 +37,33 @@ module.exports = {
         pixelId: '222946198915072',
       },
     },
-    {
-      resolve: `gatsby-plugin-facebook-analytics`, // This is going away from FB soon...useless soon!!
-      options: {
-        // Required - set this to the ID of your Facebook app.
-        appId: `611958516129057`,
+    // {
+    //   resolve: `gatsby-plugin-facebook-analytics`, // This is going away from FB soon...useless soon!!
+    //   options: {
+    //     // Required - set this to the ID of your Facebook app.
+    //     appId: `611958516129057`,
 
-        // Which version of the SDK to load.
-        version: `v7.0`,
+    //     // Which version of the SDK to load.
+    //     version: `v7.0`,
 
-        // Determines whether XFBML tags used by social plugins are parsed.
-        xfbml: true,
+    //     // Determines whether XFBML tags used by social plugins are parsed.
+    //     xfbml: true,
 
-        // Determines whether a cookie is created for the session or not.
-        cookie: false,
+    //     // Determines whether a cookie is created for the session or not.
+    //     cookie: false,
 
-        // Include Facebook analytics in development.
-        // Defaults to false meaning the library will only be loaded in production.
-        includeInDevelopment: false,
+    //     // Include Facebook analytics in development.
+    //     // Defaults to false meaning the library will only be loaded in production.
+    //     includeInDevelopment: false,
 
-        // Include debug version of sdk
-        // Defaults to false meaning the library will load sdk.js
-        debug: false,
+    //     // Include debug version of sdk
+    //     // Defaults to false meaning the library will load sdk.js
+    //     debug: false,
 
-        // Select your language.
-        language: `th_TH`,
-      },
-    },
+    //     // Select your language.
+    //     language: `th_TH`,
+    //   },
+    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-remark`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -35,11 +35,7 @@ const Layout = ({ children, header }) => {
         <> {/* Note: FB Pixel is a plugin at 'gatsby-config.js' */}
           <Helmet> {/* 1. FB SDK for app (login, etc) -- VERIFY VERSION!!!!!, 2. FB domain verification */}
             <div id="fb-root"></div>
-            <script async defer
-              crossorigin="anonymous"
-              src="https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v10.0&appId=611958516129057&autoLogAppEvents=1"
-              nonce="PFVZXkQp"
-            />
+            {/* <script async defer crossorigin="anonymous" src="https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v10.0&appId=611958516129057&autoLogAppEvents=1" nonce="PFVZXkQp" /> */}
             <meta name="facebook-domain-verification" content="k80kka3b227qj11jguub5d6jzk3ycm" />
           </Helmet>
 
